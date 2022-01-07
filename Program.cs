d2fd5ecf5c225882077e72a3c258f81013a98646
sergei-karpovic <95756026+sergei-karpovic@users.noreply.github.com>
2022-01-07 12:18:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        double X = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("{0,20}",X);
+    }
+    
+}
