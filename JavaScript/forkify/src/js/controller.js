4c2637f0dbf453aedeadc678171ca2e682be5982
sergey-karpovich <95756026+sergei-karpovic@users.noreply.github.com>
2022-09-16 10:52:40 +0300

    added new welcome feature

--- JavaScript/forkify/src/js/controller.js
+++ JavaScript/forkify/src/js/controller.js
@@ -131,6 +131,10 @@ const controlAddRecipe=async function(newRecipe){
   }
 }
 
+const newFeature=function(){
+  console.log('Welcome to the application!');
+}
+
 const init=function(){
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -139,7 +143,7 @@ const init=function(){
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);  
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 }
 
 init();
